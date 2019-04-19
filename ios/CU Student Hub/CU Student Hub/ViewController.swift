535ab6fc53367228c76918cfac2e02c38d78405d
Anthony Yang <ay339@cornell.edu>
2019-04-19 03:27:59 -0400

    Initiated the iOS portion

---
+++ ios/CU Student Hub/CU Student Hub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CU Student Hub
+//
+//  Created by Anthony Yang on 4/19/19.
+//  Copyright © 2019 Anthony Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
