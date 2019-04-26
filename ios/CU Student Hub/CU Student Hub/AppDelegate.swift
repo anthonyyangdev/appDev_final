3b5273e39cd022fb3dfea57bb741e402ee470189
Anthony Yang <ay339@cornell.edu>
2019-04-26 14:27:14 -0400

    Removed from git tree

--- ios/CU Student Hub/CU Student Hub/AppDelegate.swift
+++ ios/CU Student Hub/CU Student Hub/AppDelegate.swift
@@ -16,16 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//         Override point for customization after application launch.
+        // Override point for customization after application launch.
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = UINavigationController(rootViewController: ProfileViewController())
         window?.makeKeyAndVisible()
         return true
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
-        window?.makeKeyAndVisible()
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+//        window?.makeKeyAndVisible()
         return true    }
     }
 
