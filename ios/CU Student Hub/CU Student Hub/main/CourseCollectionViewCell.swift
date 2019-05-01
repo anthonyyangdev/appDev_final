c9472106d9779001eacf1121a85512a4a9146bbf
yc758 <yc758@cornell.edu>
2019-05-01 00:12:10 -0400

    Finished the main screen

--- ios/CU Student Hub/CU Student Hub/main/CourseCollectionViewCell.swift
+++ ios/CU Student Hub/CU Student Hub/main/CourseCollectionViewCell.swift
@@ -10,17 +10,15 @@ import UIKit
 
 class CourseCollectionViewCell: UICollectionViewCell {
     
-    var photoImageView: UIImageView!
     var name: UILabel!
     var type: UILabel!
+    var sessionCode: UILabel!
+    var location: UILabel!
     
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        photoImageView = UIImageView(frame: .zero)
-        photoImageView.translatesAutoresizingMaskIntoConstraints = false
-        photoImageView.contentMode = .scaleAspectFit
         name = UILabel(frame: .init())
         name.textColor = .black
         name.font = UIFont.systemFont(ofSize: 20, weight: .bold)
@@ -29,41 +27,57 @@ class CourseCollectionViewCell: UICollectionViewCell {
         type.textColor = .black
         type.font = UIFont.systemFont(ofSize: 15, weight: .light)
         type.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(photoImageView)
+        sessionCode = UILabel(frame: .init())
+        sessionCode.textColor = .black
+        sessionCode.font = UIFont.systemFont(ofSize: 15, weight: .light)
+        sessionCode.translatesAutoresizingMaskIntoConstraints = false
+        location = UILabel(frame: .init())
+        location.textColor = .black
+        location.font = UIFont.systemFont(ofSize: 15, weight: .light)
+        location.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(name)
         contentView.addSubview(type)
+        contentView.addSubview(sessionCode)
+        contentView.addSubview(location)
         
         setupConstraints()
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func setupConstraints(){
         NSLayoutConstraint.activate([
-            photoImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            photoImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            photoImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            photoImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -80)
-            ])
-        NSLayoutConstraint.activate([
-            name.topAnchor.constraint(equalTo: photoImageView.bottomAnchor),
+            name.topAnchor.constraint(equalTo: contentView.topAnchor),
             name.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             name.heightAnchor.constraint(equalToConstant: 20),
             name.widthAnchor.constraint(equalToConstant: 130)
             ])
         NSLayoutConstraint.activate([
-            type.topAnchor.constraint(equalTo: name.bottomAnchor),
+            sessionCode.topAnchor.constraint(equalTo: name.bottomAnchor),
+            sessionCode.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            sessionCode.heightAnchor.constraint(equalToConstant: 20),
+            sessionCode.widthAnchor.constraint(equalToConstant: 130)
+            ])
+        NSLayoutConstraint.activate([
+            type.topAnchor.constraint(equalTo: sessionCode.bottomAnchor),
             type.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
             type.heightAnchor.constraint(equalToConstant: 20),
-            type.widthAnchor.constraint(equalToConstant: 80)
+            type.widthAnchor.constraint(equalToConstant: 130)
+            ])
+        NSLayoutConstraint.activate([
+            location.topAnchor.constraint(equalTo: type.bottomAnchor),
+            location.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+            location.heightAnchor.constraint(equalToConstant: 20),
+            location.widthAnchor.constraint(equalToConstant: 130)
             ])
     }
     
-    func configure(for course: Course){
-        photoImageView.image = course.profileImage
-        name.text = course.name
-        type.text = course.type
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    func configure(for course: CornellRosterData){
+        name.text = course.descr
+        type.text = course.strm
+        sessionCode.text = course.defaultSessionCode
+        location.text = course.defaultLocation
     }
 }
--- ios/CU Student Hub/CU Student Hub/main/MainViewController.swift
+++ ios/CU Student Hub/CU Student Hub/main/MainViewController.swift
@@ -12,13 +12,13 @@ import SnapKit
 class MainViewController: UIViewController {
 
     var courseCollectionView: UICollectionView!
-    var courseArray: [Course]!
+    var courseArray: [CornellRosterData]!
     var searchTextField: UITextField!
     var searchButton: UIButton!
-    var addButton: UIButton!
-    var profileButton: UIButton!
+    var addButton: UIBarButtonItem!
+    var profileButton: UIBarButtonItem!
     
-    let photoCellReuseIdentifier = "photoCellReuseIdentifier"
+    let textCellReuseIdentifier = "textCellReuseIdentifier"
     let padding: CGFloat = 8
     let headerHeight: CGFloat = 30
     
@@ -29,6 +29,12 @@ class MainViewController: UIViewController {
         courseArray = []
         title = "Student Hub"
         view.backgroundColor = .white
+        addButton = UIBarButtonItem()
+        addButton.title = "Add"
+        self.navigationItem.rightBarButtonItem = addButton
+        profileButton = UIBarButtonItem()
+        profileButton.title = "Profile"
+        self.navigationItem.leftBarButtonItem = profileButton
         
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
@@ -40,19 +46,58 @@ class MainViewController: UIViewController {
         courseCollectionView.backgroundColor = .white
         courseCollectionView.dataSource = self
         courseCollectionView.delegate = self
-        courseCollectionView.register(CourseCollectionViewCell.self, forCellWithReuseIdentifier: photoCellReuseIdentifier)
+        courseCollectionView.register(CourseCollectionViewCell.self, forCellWithReuseIdentifier: textCellReuseIdentifier)
         view.addSubview(courseCollectionView)
         
+        searchTextField = UITextField()
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchTextField.text = "Input the course code here"
+        searchTextField.borderStyle = .roundedRect
+        searchTextField.backgroundColor = .white
+        searchTextField.textAlignment = .center
+        searchTextField.clearsOnBeginEditing = true
+        view.addSubview(searchTextField)
+        
+        searchButton = UIButton()
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.setTitle("Search", for: .normal)
+        searchButton.setTitleColor(.black, for: .normal)
+        searchButton.backgroundColor = UIColor.lightGray
+//        searchButton.addTarget(self, action: <#T##Selector#>, for: <#T##UIControl.Event#>)
+        view.addSubview(searchButton)
+        
         setupConstraints()
+        getCourses()
     }
     
     func setupConstraints() {
         NSLayoutConstraint.activate([
-            courseCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            courseCollectionView.topAnchor.constraint(equalTo: searchButton.bottomAnchor),
             courseCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            courseCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            courseCollectionView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.8),
             courseCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
             ])
+        NSLayoutConstraint.activate([
+            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchTextField.heightAnchor.constraint(equalToConstant: 40),
+            searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            ])
+        NSLayoutConstraint.activate([
+            searchButton.topAnchor.constraint(equalTo: searchTextField.bottomAnchor),
+            searchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchButton.heightAnchor.constraint(equalToConstant: 40),
+            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            ])
+    }
+    
+    func getCourses(){
+        NetworkManager.getCourses { rosterData in
+            self.courseArray = rosterData
+            DispatchQueue.main.async {
+                self.courseCollectionView.reloadData()
+            }
+        }
     }
 }
     
@@ -62,7 +107,7 @@ extension MainViewController: UICollectionViewDataSource{
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: photoCellReuseIdentifier, for: indexPath) as! CourseCollectionViewCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: textCellReuseIdentifier, for: indexPath) as! CourseCollectionViewCell
         let course = courseArray[indexPath.item]
         cell.configure(for: course)
         return cell
@@ -72,7 +117,6 @@ extension MainViewController: UICollectionViewDataSource{
 extension MainViewController: UICollectionViewDelegate{
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        // let person = peopleArray[indexPath.item]
         courseArray.remove(at: indexPath.item)
         // reload the collectionview
         collectionView.reloadData()
